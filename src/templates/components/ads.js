49b0efc777fae3677ac2f86c52ebe6bccb5914e0
JYP <tempest1033@gmail.com>
2026-01-09 22:39:28 +0900

    2026-01-09 모바일 광고 horizontal, min-height 200/500px

--- src/templates/components/ads.js
+++ src/templates/components/ads.js
@@ -19,8 +19,7 @@ function renderAdCard(slotId, options = {}) {
 
   // 타입별 AdSense 속성 추가
   if (type === 'mobile-200' || type === 'mobile-400') {
-    attrs.push('data-ad-format="auto"');
-    attrs.push('data-full-width-responsive="true"');
+    attrs.push('data-ad-format="horizontal"');
   }
   if (type === 'rectangle') {
     attrs.push('data-ad-format="rectangle"');
