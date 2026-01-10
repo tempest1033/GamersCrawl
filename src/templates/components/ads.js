4356e5f17140c6cec65fa4d2c29cdd0d37a1e410
JYP <tempest1033@gmail.com>
2026-01-11 04:49:25 +0900

    광고: data-ad-format=horizontal 적용

--- src/templates/components/ads.js
+++ src/templates/components/ads.js
@@ -27,7 +27,9 @@ function renderAdCard(slotId, options = {}) {
   ];
 
   // 타입별 AdSense 속성 추가
-  // mobile-200, mobile-400: format 미지정 (AdSense auto)
+  if (type === 'mobile-200' || type === 'mobile-400') {
+    attrs.push('data-ad-format="horizontal"');
+  }
   if (type === 'rectangle') {
     attrs.push('data-ad-format="rectangle"');
   }
